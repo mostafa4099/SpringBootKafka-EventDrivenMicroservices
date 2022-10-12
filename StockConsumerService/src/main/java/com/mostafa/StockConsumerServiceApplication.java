28f788db18ef0473a4868478f73aba5ad197cde6
mostafa4099 <mostafa.sna@gmail.com>
2022-10-12 21:10:31 +0600

    Project initialize

---
+++ StockConsumerService/src/main/java/com/mostafa/StockConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mostafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockConsumerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockConsumerServiceApplication.class, args);
+    }
+
+}
